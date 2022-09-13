60f12c13a96f25dd8b1e5f377a8eb2bf91f02349
Noah Brinkman <noahpbrinkman@gmail.com>
2022-09-13 15:12:16 +0200

    promptscenerecognition

--- Project customer/Assets/Scripts/StageScene.cs
+++ Project customer/Assets/Scripts/StageScene.cs
@@ -25,13 +25,23 @@ public class ActorScene
     
 }
 
-public class StageScene : MonoBehaviour
+[CreateAssetMenu(fileName = "StageScene", menuName = "ScriptableObjects/StageScene", order = 1)]
+public class StageScene : ScriptableObject
 {
-    [SerializeField] private string prompt = String.Empty;
-    [SerializeField] private string outcome = String.Empty;
+    public string prompt = String.Empty;
+    public string outcome = String.Empty;
     public int rows = 4;
     public int columns = 4;
     public ActorScene actorScene = new ActorScene();
+
+
+    public void Initialize()
+    {
+        if (actorScene.actors == null || actorScene.actors.Length != rows * columns)
+        {
+            actorScene.SetActorListSize(rows,columns);
+        }
+    }
     
 }
 
@@ -40,12 +50,11 @@ public class StageSceneEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        if(target == null) return;
         StageScene s = (StageScene)target;
-        
-        if (s.actorScene.actors.Length != s.rows*s.columns)
-        {
-            s.actorScene.SetActorListSize(s.rows,s.columns);
-        }
+        if(s == null) return;
+        s.Initialize();
+
         base.OnInspectorGUI();
         
        
@@ -61,7 +70,7 @@ public class StageSceneEditor : Editor
             for (int j = 0; j < s.columns; j++)
             {
                 if(index >= s.actorScene.actors.Length || index < 0) return;
-                s.actorScene.actors[index] = (Actor)EditorGUILayout.EnumPopup(s.actorScene.actors[index]);
+                s.actorScene.actors[index] = (Actor)EditorGUILayout.EnumPopup(s.actorScene.actors[index], GUILayout.Width(60), GUILayout.MaxWidth(80));
                 index++;
             }
             GUILayout.EndHorizontal();
