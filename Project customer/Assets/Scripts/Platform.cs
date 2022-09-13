2a62acf2545d9318a4bf382eccf26d5584f3f653
kama <kamila.matuszak@list.pl>
2022-09-13 14:42:40 +0200

    Colouring the platform
    Platform changes colour to show the player where the object can be placed

--- Project customer/Assets/Scripts/Platform.cs
+++ Project customer/Assets/Scripts/Platform.cs
@@ -10,14 +10,27 @@ public class Platform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        ChangeColour();
         isChosen = false;
         
+    }
+
+    private void ChangeColour()
+    {
+        if (isChosen)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.grey;
+        }
     }
 
     
