0e50492ec534a2c47e7be624e95597655e26ad25
kama <kamila.matuszak@list.pl>
2022-09-14 12:51:03 +0200

    Actor assigned to the platform

--- Project customer/Assets/Scripts/Grabber.cs
+++ Project customer/Assets/Scripts/Grabber.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 
 public class Grabber : MonoBehaviour
 {
-    private GameObject selected;
-    public GameObject spriteObject;
     private Platform platform;
     [SerializeField] private Actor myActor;
-    [SerializeField] private float heightOfPickup = 0.8f;
-    [SerializeField] private float heightOfPlatform = 1.05f;
-    [SerializeField] private float desiredScale = 1.5f;
     private float platformX;
     private float platformZ;
-    private float platformY;
     private float beginX;
     private float beginY;
     private float beginZ;
@@ -31,42 +25,8 @@ public class Grabber : MonoBehaviour
     }
     // Update is called once per frame
     void Update()
-    {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    if(selected == null)                //if there's no object selected
-        //    {
-        //        RaycastHit hit = CastRay();
-
-        //        if(hit.collider != null)        //react only if an object has a collider
-        //        {
-        //            if (!hit.collider.CompareTag("drag"))
-        //            {
-        //                return;                 //if the oobject is not dragable
-        //            }
-
-        //            selected = hit.collider.gameObject;
-        //            //Cursor.visible = false;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        spriteObject.transform.localScale = new Vector3(1, 1, 1);
-        //        DragAndDrop(heightOfPlatform, false);
-
-        //        selected = null;
-        //    }
-        //}
-
-        //if (selected != null)
-        //{
-        //    DragAndDrop(heightOfPickup, true);
-        //    CheckForPlatform();
-        //    spriteObject.transform.localScale = new Vector3(desiredScale, desiredScale, desiredScale);
-        //}
-
+    { 
         NewDragAndDrop();
-
     }
 
     private void CheckForPlatform()
@@ -77,53 +37,29 @@ public class Grabber : MonoBehaviour
         if (hit.collider.CompareTag("platform"))
         {
             Debug.Log("Platform is underneath me");
-            hasPlatform = true;
-            Debug.Log(hit.collider.gameObject.name);
-            platform = hit.collider.gameObject.GetComponent<Platform>();
-            platform.isChosen = true;
-            platformX = platform.transform.position.x;
-            platformZ = platform.transform.position.z;
-            platformY = platform.transform.position.y;
+            hasPlatform = true;   
         }
         else
         {
             hasPlatform = false;
         }
 
+        if (hasPlatform)
+        {
+            platform = hit.collider.gameObject.GetComponent<Platform>();
+            platform.isChosen = true;
+            
+            platformX = platform.transform.position.x;
+            platformZ = platform.transform.position.z;
+        }
+        else if (platform != null)
+        {
+            platform.isChosen = false;
+            platform.OnActorPickedUp(selectedTransform);
+            platform = null;
+        }
+
         Debug.DrawRay(transform.position, -Vector3.up * 5f, Color.green);
-    }
-
-    private RaycastHit CastRay()
-    {
-        Vector3 screenMousePositionFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);      //farClipPlane - futhest point for the camera to see
-        Vector3 screenMousePositionNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
-        Vector3 worldMousePositionFar = Camera.main.ScreenToWorldPoint(screenMousePositionFar);
-        Vector3 worldMousePositionNear = Camera.main.ScreenToWorldPoint(screenMousePositionNear);
-        RaycastHit hit;
-        Physics.Raycast(worldMousePositionNear, worldMousePositionFar - worldMousePositionNear, out hit);
-
-        return hit;
-    }
-
-    private void DragAndDrop(float height, bool pickup)
-    {
-        Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(selected.transform.position).z);
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
-        if (pickup)
-        {
-            selected.transform.position = new Vector3(worldPosition.x, height, worldPosition.z);
-        }
-        else if (!pickup && platform != null)
-        {
-            selected.transform.position = new Vector3(platformX, height, platformZ);
-            platform.OnActorPlaced(myActor, selected.transform);
-
-        }
-        else
-        {
-            selected.transform.position = new Vector3(beginX, beginY, beginZ);
-        }
-
     }
 
     private void NewDragAndDrop()
@@ -131,7 +67,7 @@ public class Grabber : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Debug.Log(hit.collider.name);
+            //Debug.Log(hit.collider.name);
             if (Input.GetMouseButtonDown(0))
             {
                 if (hit.collider.CompareTag("drag"))
@@ -139,17 +75,25 @@ public class Grabber : MonoBehaviour
                     selectedTransform = hit.collider.transform;
                     //Set object to ignore raycast layer
                     selectedTransform.gameObject.layer = 2;
+                    
                 }
                 else if (hasPlatform)
                 {
+                    platform.OnActorPlaced(myActor, selectedTransform);
                     selectedTransform.position = new Vector3(platformX, desiredHeightOnPlatform, platformZ);
                     selectedTransform.gameObject.layer = 0;
                     selectedTransform = null;
                 }
                 else
                 {
+                    //if (platform != null)
+                    //{
+                    //    platform.OnActorPickedUp(selectedTransform);
+                    //}
+                    selectedTransform.position = new Vector3(beginX, beginY, beginZ);
                     selectedTransform.gameObject.layer = 0;
                     selectedTransform = null;
+                    
                 }
 
             }
--- Project customer/Assets/Scripts/Platform.cs
+++ Project customer/Assets/Scripts/Platform.cs
@@ -19,15 +19,22 @@ public class Platform : MonoBehaviour
     public void OnActorPlaced(Actor actor, Transform actorTransform)
     {
         occupiedBy = actor;
+        Debug.Log(occupiedBy);
         actorTransform.LookAt(lookAtTarget, Vector3.up);
         actorTransform.parent = transform;
+    }
+
+    public void OnActorPickedUp(Transform actorTransform)
+    {
+        occupiedBy = Actor.empty;
+        actorTransform.rotation = new Quaternion(0, 0, 0,0);
+        //isChosen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         ChangeColour();
-        isChosen = false;
         
     }
 
