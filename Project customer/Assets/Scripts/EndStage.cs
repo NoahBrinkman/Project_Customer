fbfc1458d7907d1a6fb86031c673ee1aa41cf414
Kamila Matuszak <kamila.matuszak@list.pl>
2022-09-19 14:50:30 +0200

    Lights are working with colours

--- Project customer/Assets/Scripts/EndStage.cs
+++ Project customer/Assets/Scripts/EndStage.cs
@@ -16,6 +16,8 @@ public class EndStage : MonoBehaviour
     [SerializeField] private List<ActorWithPrefab> actors = new List<ActorWithPrefab>();
     Dictionary<Actor, GameObject> dictionary = new Dictionary<Actor, GameObject>();
     [SerializeField] private List<EndStageSpot> spots;
+    [SerializeField] private List<SpotlightFollow> spotlights;
+    private bool lightGreen = false;
     private void Start()
     {
         for (int i = 0; i < actors.Count; i++)
@@ -31,8 +33,12 @@ public class EndStage : MonoBehaviour
         {
             GameObject g = GameObject.Instantiate(dictionary[selection[i].actor], spots[i].transform);
             spots[i].correct = selection[i].correct;
+            lightGreen = selection[i].correct;
+            spotlights[i].endLight = true;
+            spotlights[i].greenLight = selection[i].correct;
             spots[i].endSceneDialogue = dictionary[selection[i].actor].GetComponent<Puppet>().endSceneDialogue;
             g.transform.localPosition = new Vector3(0, .5f, 0);
+
         }
         
     }
