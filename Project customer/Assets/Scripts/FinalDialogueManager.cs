4d6e402bd0d40e8216285ea2246e173faa4ce539
Noah Brinkman <noahpbrinkman@gmail.com>
2022-09-22 15:18:21 +0200

    ENding dialogue now works on an invisible score

---
+++ Project customer/Assets/Scripts/FinalDialogueManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FinalDialogueManager : MonoBehaviour
+{
+    [SerializeField, TextArea] private List<string> dialogueByScore;
+    [SerializeField] private TMP_Text text;
+
+    private void Start()
+    {
+        int amountCorrect = SelectionPasser.Instance.AmountCorrect();
+        if (amountCorrect >= 0 && amountCorrect < dialogueByScore.Count)
+        {
+            text.text = dialogueByScore[amountCorrect];
+        }
+    }
+}
