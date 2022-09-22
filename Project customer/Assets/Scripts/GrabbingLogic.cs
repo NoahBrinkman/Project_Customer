5a9e99c7f65114be90d56fcf52e23792a3affa78
Kamila Matuszak <kamila.matuszak@list.pl>
2022-09-22 14:03:54 +0200

    Pedestals on the main scene

--- Project customer/Assets/Scripts/GrabbingLogic.cs
+++ Project customer/Assets/Scripts/GrabbingLogic.cs
@@ -135,7 +135,7 @@ public class GrabbingLogic : MonoBehaviour
     private void PlaceOnThePlatform()
     {
         selectedTransform.parent = stageSpot.transform;
-        selectedTransform.localPosition = new Vector3(0, heightOffset, 0);
+        selectedTransform.localPosition = new Vector3(0, 0.45f, 0);
         selectedTransform.LookAt(new Vector3(stageSpot.lookAtTarget.position.x, selectedTransform.position.y, stageSpot.lookAtTarget.position.z), Vector3.up);
         stageSpot.occupiedBy = selectedTransform.GetComponent<GrabbableActor>().actor;
         selectedTransform.gameObject.layer = 0;                                         //Used if we want to move around actors after they've been already placed on the spot (for example: if you placed the actor on the spot 15 by accident and you want it on spot 14)
